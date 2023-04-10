7c36eab2d3acca685c1bd728d9be410c00ac5029
Бондаренко Кирилл Константинович <gr494_bkk@ttit.local>
2023-04-10 11:32:27 +0700

    Create new code in Application.php

---
+++ core/Src/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Src;
+
+use Error;
+
+class Application {
+    private Settings $settings;
+
+    public function __construct(Settings $settings) {
+        $this -> settings = $settings;
+    }
+
+    public function __get($key) {
+        if ($key === 'settings') {
+            returnb $this -> settings;
+        }
+        throw new Error('Accessing a non-existent property');
+    }
+
+    public function run(): void {
+        echo 'Working';
+    }
+}
