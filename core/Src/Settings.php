301b19d6a938cdddcac4b6280727fc87bcd5ccc3
Кирилл Бондаренко <gr494_bkk@lab-407-10.tomtit-tomsk.ru>
2023-04-11 10:20:13 +0700

    Update code this core/Src/Settings.php

--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -6,7 +6,7 @@ use Error;
 class Settings {
     private array $_settings;
 
-    public function __construct(array $_settings = []) {
+    public function __construct(array $settings = []) {
         $this -> _settings = $settings;
     }
 
