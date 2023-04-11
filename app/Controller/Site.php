f925afde27123b493b47a641b25f1444f4fa27a4
Кирилл Бондаренко <gr494_bkk@lab-407-10.tomtit-tomsk.ru>
2023-04-11 10:52:36 +0700

    Create routes/web.php and app/Controller/Site.php

---
+++ app/Controller/Site.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controller;
+
+class Site {
+
+    public function index(): void {
+        echo 'index отрабатывает штатно';
+    }
+
+    public function hello(): void {
+        echo 'hello working';
+    }
+}
