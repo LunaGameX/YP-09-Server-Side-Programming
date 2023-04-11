22e3146e8b23ef3ab91a93caeef7dfef634ea512
gr494_bkk <kirillkorovin02@yandex.ru>
2023-04-11 13:39:31 +0700

    Update code in bootstrap.php: comment out part of the code

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,20 +2,23 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
+//Подключение автозагрузчика composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
 //Добавляем пользовательскую функцию автозагрузки классов
-spl_autoload_register(function($className) {
-
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-
-        if (file_exists($fileName)) {
-            require_once $fileName;
-        }
-    }
-});
+//spl_autoload_register(function($className) {
+//
+//    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+//    $className = str_replace('\\', '/', $className);
+//
+//    foreach ($paths['classes'] as $path) {
+//        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
+//
+//        if (file_exists($fileName)) {
+//            require_once $fileName;
+//        }
+//    }
+//});
 
 //Функция, возращающая массив всех настроек приложения
 function getConfigs (string $path = DIR_CONFIG): array {
