f793966784d5ecf9c78878e40ce1bdb86625b24c
gr494_bkk <kirillkorovin02@yandex.ru>
2023-04-14 12:35:31 +0700

    YP-09-Server-Side-Programming(2) \ Update code in app/Controller/Site.php

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,12 +4,13 @@ namespace Controller;
 
 use http\Message;
 use Src\View;
+use Illuminate\Database\Capsule\Manager as DB;
 
 class Site {
 
     public function index(): string {
-        $view = new View();
-        return $view -> render('site.hello', ['message' => 'index отрабатывает штатно']);
+        $posts = DB::table('posts') -> get();
+        return (new View()) -> render('site.hello', ['message' => 'index отрабатывает штатно']);
     }
 
     public function hello(): string {
