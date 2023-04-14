3547642a4b51e6bb06173eeeffdd8893651fbe20
gr494_bkk <kirillkorovin02@yandex.ru>
2023-04-14 11:50:15 +0700

    YP-09-Server-Side-Programming(2) \ Update code in core/Src/Application.php

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -3,14 +3,19 @@
 namespace Src;
 
 use Error;
+use illuminate\Container\Container;
+use illuminate\Events\Dispatcher;
+use illuminate\Database\Capsule\Manager as Capsule;
 
 class Application {
     private Settings $settings;
     private Route $route;
+    private Capsule $dbManager;
 
     public function __construct(Settings $settings) {
         $this -> settings = $settings;
         $this -> route = new Route();
+        $this -> dbManager = new Capsule();
     }
 
     public function __get($key) {
@@ -20,7 +25,15 @@ class Application {
         throw new Error('Accessing a non-existent property');
     }
 
+    private function dbRun() {
+        $this -> dbManager -> addConnection($this -> settings -> getDbSetting());
+        $this -> dbManager -> setEventDispatcher(new Dispatcher(new Container));
+        $this -> dbManager -> setAsGlobal();
+        $this -> dbManager -> bootEloquent();
+    }
+
     public function run(): void {
+        $this -> dbRun();
         $this -> route -> setPrefix($this -> settings -> getRootPath());
         $this -> route -> start();
 //        echo 'Все отрабатывает как надо!';
