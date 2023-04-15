76ca76fca7050d36f08ecac05e6d3daadc5334e7
gr494_bkk <kirillkorovin02@yandex.ru>
2023-04-15 10:53:24 +0700

    YP-09-Server-Side-Programming(2) \ Update code is fail in directory: routes/web.php (3.13)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Src\Route;
 
 Route::add('go', [Controller\Site::class, 'index']);
+
 Route::add('hello', [Controller\Site::class, 'hello']);
 
+Route::add('signup', [Controller\Site::class, 'signup']);
